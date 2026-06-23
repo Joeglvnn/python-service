9badf121eea1c452c16fda679f014653d31b1689
Joeglvnn <jonathangalvinhansrico22@gmail.com>
2026-06-23 07:32:23 +0000

    Initial commit: Python Flask service

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return jsonify({"message": "Hello from Python Flask service!"})
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "UP"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8083)
